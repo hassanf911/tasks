b2202474b464e5fef4481991a8dd6ec2162a1313
hassan <hassanf@udel.edu>
2026-08-27 11:02:58 +0500

    Completed part 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+//import { img } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,7 +12,58 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 Mohammed Fofana UD CISC275 with React Hooks and TypeScript
                 <p> Hello World</p>
+                <div>
+                    <Button
+                        onClick={() => {
+                            //This onclick function is attached to  and in the first buton tag
+                            console.log("Hello World!");
+                        }}
+                    >
+                        Log Hello World
+                    </Button>
+                </div>
+                <div>
+                    <Container>
+                        <Row>
+                            <div
+                                style={{
+                                    backgroundColor: "red",
+                                    width: 300,
+                                    height: 400,
+                                }}
+                            >
+                                <Col>
+                                    Slipknot Songs I like.
+                                    <div>
+                                        <ul>
+                                            <li> Eyeless</li>
+                                            <li> Custer </li>
+                                            <li> Critical darling</li>
+                                        </ul>
+                                    </div>
+                                </Col>
+                            </div>
+
+                            <div
+                                style={{
+                                    backgroundColor: "blue",
+                                    width: 525,
+                                    height: 500,
+                                }}
+                            >
+                                <Col>
+                                    <img
+                                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ0Rfn9U2nzEhLdKEKeMZs-F2Ofq4EBwdR6gkxjEL0lp4fKh9oVwJu3tYW&s=10"
+                                        alt="A picture of the metal band slipknot"
+                                    />
+                                </Col>
+                            </div>
+                        </Row>
+                    </Container>
+                </div>
             </header>
+
+            <h1 style={{ backgroundColor: "Blue" }}></h1>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
